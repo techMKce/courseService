2fcea5d644a3d18f16fb899c725b7099bf2867a0
Selva2213 <selvakumar101ad@gmail.com>
2025-05-21 09:21:51 +0530

    Initial Commit

---
+++ src/test/java/com/TechM/CourseCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TechM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
